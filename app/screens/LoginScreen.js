a73169f2c0b429d039b1c02905a3c1048f771ed9
haitranho <61761632+haitranho@users.noreply.github.com>
2022-02-16 20:08:15 -0800

    adding apps folder

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  Touchable,
+} from "react-native";
+import { auth } from "../../firebase";
+import { useNavigation } from "@react-navigation/core";
+
+const LoginScreen = () => {
+  // States
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+
+  /* *
+   * Navigate to record screen if someone is logged in
+   * this is basically a listener on the Firebase server to check if a user had signed in
+   */
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigation.navigate("Record");
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  /**
+   * Handle the user login using the inputted email and password,
+   * then navigate to the record screen upon a successful authentication.
+   */
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        navigation.navigate("Record");
+      })
+      .catch((error) => alert(error.message)); // print alert for failure
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.welcome}>Login To Modio</Text>
+      <TextInput
+        style={styles.input}
+        label="Username"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      {/* Button View */}
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.userBtn} onPress={handleLogin}>
+          <Text style={styles.btnText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.userBtn}>
+          {/* Doesn't do anything right now */}
+          {/* Need to implement the sign up page for this button to work */}
+          <Text style={styles.btnText}>Signup</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "teal",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: "center",
+    color: "#fff",
+    margin: 10,
+  },
+  input: {
+    width: "90%",
+    backgroundColor: "#fff",
+    padding: 15,
+    marginBottom: 10,
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
+  },
+  userBtn: {
+    backgroundColor: "orange",
+    padding: 15,
+    width: "45%",
+  },
+  btnText: {
+    fontSize: 18,
+    textAlign: "center",
+  },
+});
+
+export default LoginScreen;
